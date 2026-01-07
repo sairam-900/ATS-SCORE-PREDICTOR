68892e250dfe760b2dcee1509be62be040e45242
NAKKA LEELA LAKSHMI SAI RAM <nleelasairamnakka@gmail.com>
2026-01-08 00:25:28 +0530

    Update server run configuration for environment variable

--- app.py
+++ app.py
@@ -215,4 +215,5 @@ def health_check():
 # RUN SERVER
 # =========================
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
